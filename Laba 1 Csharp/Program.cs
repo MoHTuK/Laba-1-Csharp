d69e9b0299c96761cbf9e3d74872355862e54f6a
Саша Ветрогон <sasha.v720@gmail.com>
2019-03-10 22:18:26 +0200

    Добавил папку с проектом

---
+++ Laba 1 Csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, result;
+            Console.WriteLine("Enter your 1 number\n");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your 2 number \n");
+            y = double.Parse(Console.ReadLine());
+            result = x + y;
+            Console.WriteLine("Your result =  " + result);
+            Console.ReadKey();
+        }
+    }
+}
